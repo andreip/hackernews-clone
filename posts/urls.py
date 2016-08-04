2043233e4ef69c32bb5a4dd8ce2d1321d9121006
Andrei Petre <p31andrei@gmail.com>
2016-08-04 12:59:08 +0300

    Add a view for posts and render nested comments as well.

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 
-from posts.views import PostListView
+from posts.views import PostListView, PostDetailView
 
 urlpatterns = [
     url(r'^$', PostListView.as_view(), name='post-list'),
-    url(r'(?P<pk>[\d+])$', PostListView.as_view(), name='post-detail'),
+    url(r'(?P<pk>[\d+])$', PostDetailView.as_view(), name='post-detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .models import Post
@@ -6,3 +7,8 @@ from .models import Post
 class PostListView(ListView):
     model = Post
     template_name = 'post_list.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
